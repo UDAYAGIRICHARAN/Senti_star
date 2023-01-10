cf22047aaeeec2e5b387ec7c351e1a3cceffbe7a
CHARAN KUMAR UDAYAGIRI <67089878+UDAYAGIRICHARAN@users.noreply.github.com>
2023-01-10 19:10:15 +0530

    Main

--- main/api/views.py
+++ main/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+import requests
+# import HTMLParser
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+def sentiment_text(request):
+    if request.method == 'POST':
+        text = request.POST.get('text')
+     
+      
+        return render(request,'dashboard/sentiment_text.html',{'result':text})
+    else:
+
+        return render(request,'dashboard/sentiment_text.html')
